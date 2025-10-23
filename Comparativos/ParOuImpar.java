bbf86381b3879a305440b590ff9aa0026839edbb
Murilo Leal <127419684+Mur1love@users.noreply.github.com>
2025-10-23 19:31:23 -0300

    Rename ParOuImpar class to Main and adjust logic
    Refactor ParOuImpar class to Main and simplify output.

--- Comparativos/ParOuImpar.java
+++ Comparativos/ParOuImpar.java
@@ -3,19 +3,18 @@ package Comparativos;
 
 import java.util.Scanner;
 
-public class ParOuImpar {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Digite um número: ");
         int n = sc.nextInt();
         sc.close();
 
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i + " é par");
-            } else {
-                System.out.println(i + " é ímpar");
-            }
+
+        if (n % 2 == 0) {
+            System.out.println(n + " é par");
+        } else {
+            System.out.println(n + " é ímpar");
         }
     }
 }
